dbb914b835bf2aef6fdc684dee1bfe1a26748545
CatInEars <89234857879@mail.ru>
2021-02-10 12:36:14 +0700

    init

--- App.tsx
+++ App.tsx
@@ -1,23 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+/*   
+*                     CatInEars             
+*       My GitHub: https://github.com/CatInEars
+*/
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>ConverTren App</Text>
+    </View>
+  );
 }
