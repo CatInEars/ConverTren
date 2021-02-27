6352fe5393008c5fed929abfd8377502d428e092
CatInEars <89234857879@mail.ru>
2021-02-27 20:45:32 +0700

    Random quote added

--- src/core/Tren/Quote.tsx
+++ src/core/Tren/Quote.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { commonStyles } from '../../common/commonStyles';
+import { getRandom } from '../../modules/getRandom';
 import { quoteArr } from '../../modules/quoteArr';
 
 export function Quote() {
+  const [randomQuote, setRandomQuote] = useState(
+    getRandom(0, quoteArr.length)
+  );
+
   return (
     <View
       style={commonStyles.trenQuoteContainer}
@@ -15,7 +20,7 @@ export function Quote() {
             fontStyle: 'italic'
           }}
         >
-          {quoteArr[1].quote}
+          {quoteArr[randomQuote].quote}
         </Text>
       </View>
 
@@ -25,7 +30,7 @@ export function Quote() {
         <Text
           style={commonStyles.trenQuoteText}
         >
-          {quoteArr[1].author}
+          {quoteArr[randomQuote].author}
         </Text>
       </View>
     </View>
